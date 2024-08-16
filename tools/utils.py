c67c022c2521524786623698fd61d00edc675117
Xu, Rui <rui.xu@intel.com>
2024-08-16 01:34:24 -0700

    fix

--- tools/utils.py
+++ tools/utils.py
@@ -27,6 +27,7 @@ from gc_mlir.runtime.np_to_memref import (
     get_ranked_memref_descriptor,
     make_nd_memref_descriptor,
 )
+import math
 
 MLIR_TYPE_TO_NUMPY_TYPE = {
     "bf16": ml_dtypes.bfloat16,
@@ -124,12 +125,23 @@ def get_mlir_args(
     return compiled_func_args
 
 
-def make_mlir_ndarray(mlir_type):
-    """create numpy ndarray from mlir type"""
-    return np.zeros(
-        mlir_type.shape, MLIR_TYPE_TO_NUMPY_TYPE[str(mlir_type.element_type)]
-    )
+# def make_mlir_ndarray(mlir_type):
+#     """create numpy ndarray from mlir type"""
+#     return np.zeros(
+#         mlir_type.shape, MLIR_TYPE_TO_NUMPY_TYPE[str(mlir_type.element_type)]
+#     )
 
+def make_mlir_ndarray(tensor_type):
+    shape = tensor_type.shape
+    dtype = MLIR_TYPE_TO_NUMPY_TYPE[str(tensor_type.element_type)]
+    dtypeSize = np.dtype(dtype).itemsize
+    raw_size = math.prod(shape)
+    size = int(raw_size + 64 / dtypeSize)
+    raw = np.empty(size, dtype)
+    rawptr, read_only_fla = raw.__array_interface__['data']
+    offset = int((64 - rawptr % 64) / dtypeSize)
+    result = raw[offset:offset+raw_size].reshape(shape)
+    return result
 
 def get_kernel_func_from_module(
     module: ir.Module, func_name: str = "main_entry"
